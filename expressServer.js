ed1b4329f1d1b5a5e800b8d2d9622ff04a6da521
ikm216 <ikmclassof22@gmail.com>
2024-12-07 20:58:48 -0500

    feat: added documentation to make a express server with hello world

--- expressServer.js
+++ expressServer.js
@@ -1,6 +1,18 @@
+/**
+ * @file Express server implementation.
+ * @description A simple Express server with a root endpoint that responds with "hello world".
+ */
+
 const express = require('express')
 const app = express();
 
+/**
+ * Root endpoint that responds with "hello world".
+ *
+ * @param {Request} req - The Express request object.
+ * @param {Response} res - The Express response object.
+ * @returns {void}
+ */
 app.get('/', (req, res) => {
     res.send('hello world');
 });
